137522f000289025fc3810ced361fe1ed42129ac
lmoraesdev <lbc_moraes@outlook.com>
2022-09-05 23:21:00 -0300

    feat: logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,9 +1,12 @@
 import style from "./Cardapio.module.scss";
+import {ReactComponent as Logo} from "../../assets/logo.svg";
 
 export default function Cardapio() {
 	return (
 		<main>
-			<nav className={style.menu}>Aluroni</nav>
+			<nav className={style.menu}>
+				<Logo />
+			</nav>
 		</main>
 	);
 }
