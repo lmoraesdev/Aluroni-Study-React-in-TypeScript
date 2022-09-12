2ae00cf7f3820917d3abbc79a6b68b418b296dce
lmoraesdev <lbc_moraes@outlook.com>
2022-09-12 20:55:35 -0300

    feat: buscador e refatoração do module scss

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,10 @@
 import style from "./Cardapio.module.scss";
 import {ReactComponent as Logo} from "assets/logo.svg";
+import Buscador from "./Buscador"
+import { useState } from "react";
 
 export default function Cardapio() {
+	const [busca, setBusca] = useState("");
 	return (
 		<main>
 			<nav className={style.menu}>
@@ -12,6 +15,13 @@ export default function Cardapio() {
 					A Casa do código e da massa
 				</div>
 			</header>
+			<section className={style.cardapio}>
+				<h3 className={style.cardapio__titulo}>Cardápio</h3>
+				<Buscador
+					busca={busca}
+					setBusca={setBusca}
+				/>
+			</section>
 		</main>
 	);
 }
