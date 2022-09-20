ca83fb91a1dcfddd45e12bac52d96eb0672208ad
lmoraesdev <lbc_moraes@outlook.com>
2022-09-20 14:35:02 -0300

    feat: classanames dinamicas

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import style from "./Item.module.scss";
 import cardapio from "../itens.json";
+import classnames from "classnames";
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
-	const { title, description, category, size, serving, price } = props;
+	const { title, description, category, size, serving, price, photo } = props;
 	return (
 		<div className={style.item}>
 			<div className={style.item__imagem}>
-				<img src="" alt={title} />
+				<img src={photo} alt={title} />
 			</div>
 			<div className={style.item__descricao}>
 				<div className={style.item__titulo}>
@@ -17,7 +18,14 @@ export default function Item(props: Props) {
 					<p>{description}</p>
 				</div>
 				<div className={style.item__tags}>
-					<div className={style.item__tipo}>{category.label}</div>
+					<div
+						className={classnames({
+							[style.item__tipo]: true,
+							[style[`item__tipo__${category.label.toLowerCase()}`]]: true,
+						})}
+					>
+						{category.label}
+					</div>
 					<div className={style.item__porcao}>{size}</div>
 					<div className={style.item__qtdpessoas}>
 						Serve {serving} Pessoa{serving === 1 ? "" : "s"}
