41e8e0711b9a08d5b2c2042106ec0a3422c71b6b
lmoraesdev <lbc_moraes@outlook.com>
2022-09-20 13:39:36 -0300

    feat: item e scss

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,10 +1,24 @@
-import React from "react"
-import Style from "./Item.module.scss"
+import React from "react";
+import style from "./Item.module.scss";
 
-export default function Item(){
-	return(
-		<div>Item
-
+export default function Item() {
+	return (
+		<div className={style.item}>
+			<div className={style.item__imagem}>
+				<img src="" alt="imagem" />
+			</div>
+			<div className={style.item__descricao}>
+				<div className={style.item__titulo}>
+					<h2></h2>
+					<p></p>
+				</div>
+				<div className={style.item__tags}>
+					<div className={style.item__tipo}></div>
+					<div className={style.item__porcao}></div>
+					<div className={style.item__qtdpessoas}></div>
+					<div className={style.item_valor}></div>
+				</div>
+			</div>
 		</div>
-	)
+	);
 }
--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import style from "./Itens.module.scss"
-import cardapio from "./itens.json"
-import Item from "./Item"
+import style from "./Itens.module.scss";
+import cardapio from "./itens.json";
+import Item from "./Item";
 
-
-export default function Itens(){
+export default function Itens() {
 	return (
-		<div>
-			{cardapio.map(item => (
+		<div className={style.itens}>
+			{cardapio.map((item) => (
 				<Item key={item.id} />
 			))}
 		</div>
-
-	)
+	);
 }
